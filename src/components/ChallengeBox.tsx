8f2afdf7af1b8808a775a6a55821935cb599a0e1
João <joaomsfreis@gmail.com>
2021-02-25 20:27:44 -0300

    Fourth day

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,9 +1,21 @@
 import {useContext} from "react";
 import styles from '../styles/components/ChallengeBox.module.css';
 import {ChallengesContext} from "../contexts/ChallengesContext";
+import {CountdownContext} from "../contexts/CountdownContext";
 
 export function ChallengeBox() {
-    const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+    const { activeChallenge, resetChallenge, completedChallenge } = useContext(ChallengesContext);
+    const { resetCountDown } = useContext(CountdownContext);
+
+    function handleChallengeSucceeded() {
+        completedChallenge();
+        resetCountDown();
+    }
+
+    function handleChallengeFailed() {
+        resetChallenge();
+        resetCountDown();
+    }
 
     return (
         <div className={styles.challengeBoxContainer}>
@@ -21,13 +33,14 @@ export function ChallengeBox() {
                         <button
                             type="button"
                             className={styles.challengeFailedButton}
-                            onClick={resetChallenge}
+                            onClick={handleChallengeFailed}
                         >
                             Falhei
                         </button>
                         <button
                             type="button"
                             className={styles.challengeSucceededButton}
+                            onClick={handleChallengeSucceeded}
                         >
                             Completei
                         </button>
